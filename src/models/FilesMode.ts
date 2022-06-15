e2dd433ade0f0bbd7c1437daebd130ecf485dac9
SetMod <voroncka.andrey@gmail.com>
2022-06-15 16:53:44 +0300

    refactor: deleted unused files

--- src/models/FilesMode.ts
+++
@@ -1,8 +0,0 @@
-export default class Files {
-    fileId: number | undefined
-    fileName: string | undefined
-    fileCreateDate: Date | undefined
-    filePath: string | undefined
-    userId: number | undefined
-    organizationId: number | undefined
-}
