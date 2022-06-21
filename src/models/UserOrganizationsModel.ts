a85e32a5fad7a4e0e6ff11690bf017908a73993b
SetMod <voroncka.andrey@gmail.com>
2022-06-21 09:42:43 +0300

    feat: created user organizations model

---
+++ src/models/UserOrganizationsModel.ts
@@ -0,0 +1,10 @@
+export default class UserOrganizations {
+    organizationId: number = 0
+    organizationName: string = ''
+    organizationDescription: string = ''
+    organizationEmail: string = ''
+    organizationPhone: string = ''
+    organizationCreateDate: Date = new Date()
+    organizationRoleName: string = ''
+    organizationRoleId: number = 0
+}
