61289e0fef0075b8c07144961f16b84991d1b937
SetMod <voroncka.andrey@gmail.com>
2022-06-25 19:55:18 +0300

    update: updated files component

--- src/components/Files/FilesDataTable.vue
+++ src/components/Files/FilesDataTable.vue
@@ -17,12 +17,36 @@
         </template>
         <Column field="fileId" header="ID" :sortable="true"></Column>
         <Column field="fileName" header="Name" :sortable="true"></Column>
-        <Column field="fileCreateDate" header="CreateDate" :sortable="true"></Column>
-        <Column field="userId" header="User"></Column>
-        <Column field="organizationId" header="Organization" :sortable="true"></Column>
+        <Column field="fileCreateDate" header="CreateDate" :sortable="true">
+            <template #body="slotProps">
+                <div>
+                    {{ new Date(slotProps.data.fileCreateDate).toLocaleDateString() }}
+                </div>
+            </template>
+        </Column>
+        <Column field="userId" header="User" :sortable="true" style="min-width:16rem">
+            <template #body="slotProps">
+                <div>
+                    {{ users?.find((usr) => usr.userId ==
+                            slotProps.data.userId)?.userFirstName || slotProps.data.userId
+                    }}
+                </div>
+            </template>
+        </Column>
+        <Column field="organizationId" header="Organization" :sortable="true" style="min-width:16rem">
+            <template #body="slotProps">
+                <div>
+                    {{ userOrganizations?.find((org) => org.organizationId ==
+                            slotProps.data.organizationId)?.organizationName || slotProps.data.organizationId
+                    }}
+                </div>
+            </template>
+        </Column>
         <Column header-style="width: 4rem; text-align: center" body-style="text-align: center; overflow: visible">
             <template #body="slotProps">
                 <div class="flex justify-content-between align-content-center">
+                    <!-- <Button type="button" class="p-button-outlined p-button-info mr-3" icon="pi pi-eye"
+                        @click="() => openView(slotProps.data)"></Button> -->
                     <Button type="button" class="p-button-outlined p-button-danger mr-3" icon="pi pi-times"
                         @click="() => openDelete(slotProps.data)"></Button>
                     <Button type="button" class="p-button-outlined p-button-warning mr-3" icon="pi pi-pencil"
@@ -42,6 +66,8 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue"
+import useOrganizations from "../../hooks/useOrganizations"
+import useUsers from "../../hooks/useUsers"
 import Files from "../../models/FilesModel"
 
 export default defineComponent({
@@ -68,8 +94,12 @@ export default defineComponent({
         },
     },
     setup(props) {
+        const { users } = useUsers()
+        const { userOrganizations } = useOrganizations()
         return {
             props,
+            users,
+            userOrganizations,
         }
     }
 })
