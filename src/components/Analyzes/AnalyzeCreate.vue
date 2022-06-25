343c7161f1b26095601353d5cfd61b0761a6dd00
SetMod <voroncka.andrey@gmail.com>
2022-06-25 19:54:52 +0300

    feat: added new components for analyze

--- src/components/Analyzes/AnalyzeCreate.vue
+++ src/components/Analyzes/AnalyzeCreate.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="p-card m-auto my-5 w-30rem">
+    <div class="p-card m-5 w-30rem">
         <h2 class="p-card-title">Analyze</h2>
         <div class="p-card-body  text-left">
             <div class="field flex flex-column flex flex-column">
@@ -26,7 +26,7 @@
             <div class="field flex flex-column">
                 <label for="name">Support:</label>
                 <InputNumber id="name" v-model.number="v$.analyzeSupport.$model" required="true" autofocus
-                    :class="v$.analyzeSupport.$invalid ? 'p-invalid' : ''" :min="0.1" :max="1" :step="0.1" />
+                    :class="v$.analyzeSupport.$invalid ? 'p-invalid' : ''" :min="0.01" :max="1" :step="0.1" />
                 <small v-if="v$.analyzeSupport.required.$invalid" class="p-error">Support is required.</small>
                 <small v-else-if="v$.analyzeSupport.between.$invalid" class="p-error">{{
                         v$.analyzeSupport.between.$message.replace('The value', 'Support')
@@ -35,7 +35,7 @@
             <div class="field flex flex-column">
                 <label for="name">Lift:</label>
                 <InputNumber id="name" v-model.number="v$.analyzeLift.$model" required="true" autofocus
-                    :class="v$.analyzeLift.$invalid ? 'p-invalid' : ''" :min="0.1" :max="1" :step="0.1" />
+                    :class="v$.analyzeLift.$invalid ? 'p-invalid' : ''" :min="0.01" :max="1" :step="0.1" />
                 <small v-if="v$.analyzeLift.required.$invalid" class="p-error">Lift is required.</small>
                 <small v-else-if="v$.analyzeLift.between.$invalid" class="p-error">{{
                         v$.analyzeLift.between.$message.replace('The value', 'Lift')
@@ -44,7 +44,7 @@
             <div class="field flex flex-column">
                 <label for="name">Confidence:</label>
                 <InputNumber id="name" v-model.number="v$.analyzeConfidence.$model" required="true" autofocus
-                    :class="v$.analyzeConfidence.$invalid ? 'p-invalid' : ''" :min="0.1" :max="1" :step="0.1" />
+                    :class="v$.analyzeConfidence.$invalid ? 'p-invalid' : ''" :min="0.01" :max="1" :step="0.1" />
                 <small v-if="v$.analyzeConfidence.required.$invalid" class="p-error">Confidence is required.</small>
                 <small v-else-if="v$.analyzeConfidence.between.$invalid" class="p-error">{{
                         v$.analyzeConfidence.between.$message.replace('The value', 'Confidence')
@@ -76,7 +76,8 @@
                 <Button v-else label="Create report" class="p-button-link" @click="redirectReports" />
             </div>
             <div class="field flex justify-content-around">
-                <Button label="Analyze" :disabled="!selectedFile || !selectedReport"
+                <i v-if="loading" class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
+                <Button v-else label="Analyze" :disabled="!selectedFile || !selectedReport"
                     @click="() => submit(state, selectedFile, selectedReport)" />
             </div>
         </div>
@@ -96,6 +97,10 @@ import Reports from "../../models/ReportsModel"
 
 export default defineComponent({
     props: {
+        loading: {
+            type: Boolean,
+            default: false
+        },
         submit: {
             type: Function,
             required: true
@@ -122,9 +127,9 @@ export default defineComponent({
         const rules = {
             analyzeName: { required, minLength: minLength(2), maxLength: maxLength(200) },
             analyzeDescription: { required, minLength: minLength(10), maxLength: maxLength(2000) },
-            analyzeSupport: { required, decimal, between: between(0.1, 1) },
-            analyzeLift: { required, decimal, between: between(0.1, 1) },
-            analyzeConfidence: { required, decimal, between: between(0.1, 1) },
+            analyzeSupport: { required, decimal, between: between(0.01, 1) },
+            analyzeLift: { required, decimal, between: between(0.01, 1) },
+            analyzeConfidence: { required, decimal, between: between(0.01, 1) },
             analyzeRulesLength: { required, decimal, between: between(1, 10) },
         }
         const v$ = useVuelidate(rules, state)
