3ae95892b2f67820fc570e981b10b3e50a54c087
SetMod <voroncka.andrey@gmail.com>
2022-06-26 00:29:20 +0300

    update: changed weib history, gor rid off # in url

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import Files from '../views/Files.vue'
 import Analyze from '../views/Analyze.vue'
@@ -75,7 +75,7 @@ export const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
