816ddcc04247fb7c642c371799bda8a22a428cfe
SetMod <voroncka.andrey@gmail.com>
2023-12-02 20:30:02 +0200

    Updated config.ts for jwt

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,31 @@
+import axios from "axios"
+
+export const accessTokenKey = "access_token"
+
 const config = {
-    baseUrl: 'http://localhost:5000/api/v1'
+  baseUrl: 'http://localhost:5000/api/v1',
+  withCredentials: true,
+  headers: {
+    "Content-type": "application/json",
+  },
 }
+
+export const api = axios.create({
+  baseURL: config.baseUrl,
+})
+
+// Axios interceptor to include JWT in request headers
+api.interceptors.request.use(
+  (config) => {
+    const access_token = localStorage.getItem(accessTokenKey);
+    if (access_token) {
+      config.headers.Authorization = `Bearer ${access_token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default config
