47c06cf2574166795d2be1274a8557ffa3ef0d50
SetMod <voroncka.andrey@gmail.com>
2023-12-04 00:10:44 +0200

    Updated config.ts file

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
-import axios from "axios"
-
-export const accessTokenKey = "access_token"
+import axios, { AxiosError } from "axios"
+import router from "./router"
+export const ACCESS_TOKEN_STORAGE_KEY = "access_token"
 
 const config = {
   baseUrl: 'http://localhost:5000/api/v1',
@@ -17,14 +17,30 @@ export const api = axios.create({
 // Axios interceptor to include JWT in request headers
 api.interceptors.request.use(
   (config) => {
-    const access_token = localStorage.getItem(accessTokenKey);
+    const access_token = localStorage.getItem(ACCESS_TOKEN_STORAGE_KEY);
     if (access_token) {
       config.headers.Authorization = `Bearer ${access_token}`;
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
+  (err: AxiosError) => {
+    return Promise.reject(err);
+  }
+);
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err instanceof AxiosError) {
+      if (err.response && err.response.status == 401) {
+        router.push({
+          path: '/login'
+        })
+      }
+      return Promise.reject(err);
+    } else {
+      return Promise.reject(err);
+    }
   }
 );
 
