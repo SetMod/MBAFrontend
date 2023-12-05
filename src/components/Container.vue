ed9e243fbcba284d4714d6d4ff71a9d938b829ed
SetMod <voroncka.andrey@gmail.com>
2023-12-05 19:15:14 +0200

    Updated container component

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import useRoutes from "../hooks/useRoutes"
-
-const { breadCrumbRoutes } = useRoutes()
 </script>
 
 <template>
-    <div class="w-screen">
-      <Breadcrumb :home="breadCrumbRoutes[0]" :model="breadCrumbRoutes" />
-        <slot />
-    </div>
+  <div class="w-full h-screen">
+    <slot />
+  </div>
 </template>
