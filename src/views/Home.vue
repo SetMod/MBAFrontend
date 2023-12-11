213b0c6eb53e87df316acd8092266a8939fb5d54
SetMod <voroncka.andrey@gmail.com>
2023-12-11 16:18:07 +0200

    Updated home and register views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,5 @@
 <script setup lang="ts">
-import useUsers from "../hooks/useUsers";
-import useRoutes from "../hooks/useRoutes";
-
-const { isLoggedIn } = useUsers()
-const {  redirectLogin} = useRoutes()
-// const getStarted = () => {
-//   isLoggedIn.value ? redirectFiles() : redirectLogin()
-// }
 </script>
-
 
 <template>
   <div class="mt-8">
@@ -17,4 +8,3 @@ const {  redirectLogin} = useRoutes()
     <!-- <Button class="mt-5" @click="getStarted">Get started</Button> -->
   </div>
 </template>
-
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import Users from "../models/UsersModel";
-import useUsers, { useUserRegisterValidate } from "../hooks/useUsers";
+import useUsers, { useUserCreateValidate } from "../hooks/useUsers";
 import useRoutes from "../hooks/useRoutes";
 import { useToast } from "primevue/usetoast";
 
 const { redirectLogin } = useRoutes()
 const { register, usersError } = useUsers()
-const { registerValidate, registerState } = useUserRegisterValidate()
+const { userCreateValidate, userCreateState } = useUserCreateValidate()
 
 const toast = useToast()
 const submitted = ref(false);
@@ -15,19 +15,19 @@ const submitted = ref(false);
 const submitSignUp = async () => {
   submitted.value = true;
 
-  if (registerValidate.value.$invalid) {
+  if (userCreateValidate.value.$invalid) {
     toast.add({ severity: 'warn', summary: 'Warning', detail: 'Validate all fields', life: 3000 })
     return
   }
 
   const newUser = new Users()
-  newUser.firstName = registerState.firstName
-  newUser.secondName = registerState.secondName
-  newUser.email = registerState.email
-  newUser.username = registerState.username
-  newUser.passwordHash = registerState.password
-  newUser.phone = registerState.phone
-  newUser.role = registerState.role
+  newUser.firstName = userCreateState.firstName
+  newUser.secondName = userCreateState.secondName
+  newUser.email = userCreateState.email
+  newUser.username = userCreateState.username
+  newUser.passwordHash = userCreateState.password
+  newUser.phone = userCreateState.phone
+  newUser.role = userCreateState.role
 
   await register(newUser)
 
@@ -50,91 +50,91 @@ const submitSignUp = async () => {
         <div class="p-fluid text-left">
           <div class="field">
             <label>First name:</label>
-            <InputText v-model="registerValidate.firstName.$model"
-              :class="{ 'p-invalid': registerValidate.firstName.$invalid && submitted }" placeholder="first name" />
-            <small v-if="(registerValidate.firstName.$invalid && submitted) || registerValidate.firstName.$pending"
+            <InputText v-model="userCreateValidate.firstName.$model"
+              :class="{ 'p-invalid': userCreateValidate.firstName.$invalid && submitted }" placeholder="first name" />
+            <small v-if="(userCreateValidate.firstName.$invalid && submitted) || userCreateValidate.firstName.$pending"
               class="p-error">{{
-                registerValidate.firstName.required.$message.replace('Value', 'First name')
+                userCreateValidate.firstName.required.$message.replace('Value', 'First name')
               }}</small>
           </div>
 
           <div class="field">
             <label>Second name:</label>
-            <InputText v-model="registerValidate.secondName.$model"
-              :class="{ 'p-invalid': registerValidate.secondName.$invalid && submitted }" placeholder="second name" />
-            <small v-if="(registerValidate.secondName.$invalid && submitted) || registerValidate.secondName.$pending"
+            <InputText v-model="userCreateValidate.secondName.$model"
+              :class="{ 'p-invalid': userCreateValidate.secondName.$invalid && submitted }" placeholder="second name" />
+            <small v-if="(userCreateValidate.secondName.$invalid && submitted) || userCreateValidate.secondName.$pending"
               class="p-error">{{
-                registerValidate.secondName.required.$message.replace('Value', 'Second name')
+                userCreateValidate.secondName.required.$message.replace('Value', 'Second name')
               }}</small>
           </div>
 
           <div class="field">
             <label>E-mail:</label>
-            <InputText v-model="registerValidate.email.$model"
-              :class="{ 'p-invalid': registerValidate.email.$invalid && submitted }" placeholder="email@example.com"
+            <InputText v-model="userCreateValidate.email.$model"
+              :class="{ 'p-invalid': userCreateValidate.email.$invalid && submitted }" placeholder="email@example.com"
               type="email" />
-            <span v-if="registerValidate.email.$error && submitted">
-              <span v-for="(error, index) of registerValidate.email.$errors" id="email-error" :key="index">
+            <span v-if="userCreateValidate.email.$error && submitted">
+              <span v-for="(error, index) of userCreateValidate.email.$errors" id="email-error" :key="index">
                 <small class="p-error">{{ error.$message }}</small>
               </span>
             </span>
-            <small v-else-if="(registerValidate.email.$invalid && submitted) || registerValidate.email.$pending"
+            <small v-else-if="(userCreateValidate.email.$invalid && submitted) || userCreateValidate.email.$pending"
               class="p-error">{{
-                registerValidate.email.required.$message.replace('Value', 'E-mail')
+                userCreateValidate.email.required.$message.replace('Value', 'E-mail')
               }}</small>
           </div>
 
           <div class="field">
             <label>Phone number:</label>
-            <InputMask v-model="registerValidate.phone.$model" mask="+99 (999) 999-9999"
+            <InputMask v-model="userCreateValidate.phone.$model" mask="+99 (999) 999-9999"
               placeholder="+ 99 (999) 999-9999" />
-            <small v-if="(registerValidate.phone.required.$invalid && submitted) || registerValidate.phone.$pending"
+            <small v-if="(userCreateValidate.phone.required.$invalid && submitted) || userCreateValidate.phone.$pending"
               class="p-error">{{
-                registerValidate.phone.required.$message.replace('Value', 'Phone number')
+                userCreateValidate.phone.required.$message.replace('Value', 'Phone number')
               }}</small>
-            <small v-else-if="registerValidate.phone.minLength.$invalid && submitted" class="p-error">{{
-              registerValidate.phone.minLength.$message.replace('Value', 'Phone number')
+            <small v-else-if="userCreateValidate.phone.minLength.$invalid && submitted" class="p-error">{{
+              userCreateValidate.phone.minLength.$message.replace('Value', 'Phone number')
             }}</small>
-            <small v-else-if="registerValidate.phone.maxLength.$invalid && submitted" class="p-error">{{
-              registerValidate.phone.maxLength.$message.replace('Value', 'Phone number')
+            <small v-else-if="userCreateValidate.phone.maxLength.$invalid && submitted" class="p-error">{{
+              userCreateValidate.phone.maxLength.$message.replace('Value', 'Phone number')
             }}</small>
           </div>
 
           <div class="field">
             <label>Username:</label>
-            <InputText v-model="registerValidate.username.$model"
-              :class="{ 'p-invalid': registerValidate.username.$invalid && submitted }" placeholder="username" />
-            <small v-if="registerValidate.username.required.$invalid && submitted" class="p-error">{{
-              registerValidate.username.required.$message.replace('Value', 'Username')
+            <InputText v-model="userCreateValidate.username.$model"
+              :class="{ 'p-invalid': userCreateValidate.username.$invalid && submitted }" placeholder="username" />
+            <small v-if="userCreateValidate.username.required.$invalid && submitted" class="p-error">{{
+              userCreateValidate.username.required.$message.replace('Value', 'Username')
             }}</small>
-            <small v-else-if="registerValidate.username.minLength.$invalid && submitted" class="p-error">{{
-              registerValidate.username.minLength.$message.replace('This field', 'Username')
+            <small v-else-if="userCreateValidate.username.minLength.$invalid && submitted" class="p-error">{{
+              userCreateValidate.username.minLength.$message.replace('This field', 'Username')
             }}</small>
           </div>
 
           <div class="field">
             <label>Password:</label>
-            <Password v-model="registerValidate.password.$model"
-              :class="{ 'p-invalid': registerValidate.password.$invalid && submitted }" placeholder="password"
+            <Password v-model="userCreateValidate.password.$model"
+              :class="{ 'p-invalid': userCreateValidate.password.$invalid && submitted }" placeholder="password"
               toggle-mask />
-            <small v-if="(registerValidate.password.required.$invalid && submitted) || registerValidate.password.$pending"
+            <small v-if="(userCreateValidate.password.required.$invalid && submitted) || userCreateValidate.password.$pending"
               class="p-error">{{
-                registerValidate.password.required.$message.replace('Value', 'Password')
+                userCreateValidate.password.required.$message.replace('Value', 'Password')
               }}</small>
           </div>
 
           <div class="field">
             <label>Confirm password:</label>
-            <Password v-model="registerValidate.confirmPassword.$model"
-              :class="{ 'p-invalid': registerValidate.confirmPassword.$invalid && submitted }" placeholder="password"
+            <Password v-model="userCreateValidate.confirmPassword.$model"
+              :class="{ 'p-invalid': userCreateValidate.confirmPassword.$invalid && submitted }" placeholder="password"
               toggle-mask :feedback="submitted" />
             <small
-              v-if="(registerValidate.confirmPassword.required.$invalid && submitted) || registerValidate.confirmPassword.$pending"
+              v-if="(userCreateValidate.confirmPassword.required.$invalid && submitted) || userCreateValidate.confirmPassword.$pending"
               class="p-error">{{
-                registerValidate.confirmPassword.required.$message.replace('Value', 'Password')
+                userCreateValidate.confirmPassword.required.$message.replace('Value', 'Password')
               }}</small>
             <small
-              v-else-if="(registerValidate.confirmPassword.sameAsPassword.$invalid && submitted) || registerValidate.confirmPassword.$pending"
+              v-else-if="(userCreateValidate.confirmPassword.sameAsPassword.$invalid && submitted) || userCreateValidate.confirmPassword.$pending"
               class="p-error">Password doesn't match</small>
           </div>
 
